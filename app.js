875587c0ce579a7335906891a97d753da45c8a39
Gerard <gkhynes@gmail.com>
2017-12-03 16:39:55 +0000

    Style castles page

--- app.js
+++ app.js
@@ -20,6 +20,36 @@ var castles = [
     name: "Roscommon Castle",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/5/52/Roscommon_Castle.JPG"
+  },
+  {
+    name: "Dunluce Castle",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Dunluce_Castle.jpg/256px-Dunluce_Castle.jpg"
+  },
+  {
+    name: "Dunguaire Castle",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6b/Dunguaire_Castle%2C_Galway%2C_Ireland.png"
+  },
+  {
+    name: "Roscommon Castle",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/52/Roscommon_Castle.JPG"
+  },
+  {
+    name: "Dunluce Castle",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Dunluce_Castle.jpg/256px-Dunluce_Castle.jpg"
+  },
+  {
+    name: "Dunguaire Castle",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6b/Dunguaire_Castle%2C_Galway%2C_Ireland.png"
+  },
+  {
+    name: "Roscommon Castle",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/52/Roscommon_Castle.JPG"
   }
 ];
 
