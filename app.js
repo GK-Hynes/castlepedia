d0fa711a9d3b9b8b10f860d7955a9dbe73c91ba9
Gerard <gkhynes@gmail.com>
2017-12-03 15:14:57 +0000

    Add landing page and castles page

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.send("Landing page here");
+  res.render("landing");
+});
+
+app.get("/castles", function(req, res) {
+  var castles = [
+    {
+      name: "Dunluce Castle",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Dunluce_Castle.jpg/256px-Dunluce_Castle.jpg"
+    },
+    {
+      name: "Dunguaire Castle",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/6/6b/Dunguaire_Castle%2C_Galway%2C_Ireland.png"
+    },
+    {
+      name: "Roscommon Castle",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/5/52/Roscommon_Castle.JPG"
+    }
+  ];
+  res.render("castles", { castles: castles });
 });
 
 app.listen(3000, function() {
-  console.log("The Castlepedia server has started");
+  console.log("The Castlepedia server has started.");
 });
