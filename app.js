85e7c94913788cb5e903cc13afe5d34774fd8db1
Gerard <gkhynes@gmail.com>
2017-12-09 11:11:58 +0000

    Add Castle Show Page

--- app.js
+++ app.js
@@ -10,31 +10,53 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var castleSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Castle = mongoose.model("Castle", castleSchema);
+
+// Castle.create(
+//   {
+//     name: "Dunluce Castle",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/e/e8/Dunluce_Castle.jpg",
+//     description:
+//       "Dunluce Castlen is a now-ruined medieval castle in Northern Ireland."
+//   },
+//   function(err, castle) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Newly created castle:");
+//       console.log(castle);
+//     }
+//   }
+// );
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
+//INDEX - show all castles
 app.get("/castles", function(req, res) {
   // Get all castles from DB
   Castle.find({}, function(err, allCastles) {
     if (err) {
       console.log(err);
     } else {
-      res.render("castles", { castles: allCastles });
+      res.render("index", { castles: allCastles });
     }
   });
 });
 
+//CREATE - add new castles
 app.post("/castles", function(req, res) {
   // get data from form and add to castles array
   var name = req.body.name;
   var image = req.body.image;
-  var newCastle = { name: name, image: image };
+  var desc = req.body.description;
+  var newCastle = { name: name, image: image, description: desc };
   // Create a new castle and save to DB
   Castle.create(newCastle, function(err, newlyCreated) {
     if (err) {
@@ -46,8 +68,22 @@ app.post("/castles", function(req, res) {
   });
 });
 
+//NEW - show form to create new castle
 app.get("/castles/new", function(req, res) {
   res.render("new.ejs");
+});
+
+//SHOW - shows more info about one castle
+app.get("/castles/:id", function(req, res) {
+  // find the castle with the provided id
+  Castle.findById(req.params.id, function(err, foundCastle) {
+    if (err) {
+      console.log(err);
+    } else {
+      // render show template with that castle
+      res.render("show", { castle: foundCastle });
+    }
+  });
 });
 
 app.listen(3000, function() {
