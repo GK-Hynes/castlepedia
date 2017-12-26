5ebf2918f91b0913bf1dd750ff483990e33d9826
Gerard <gkhynes@gmail.com>
2017-12-26 23:13:05 +0000

    Add comment delete route

--- routes/comments.js
+++ routes/comments.js
@@ -71,6 +71,17 @@ router.put("/:comment_id", function(req, res) {
   });
 });
 
+// Destroy Comments
+router.delete("/:comment_id", function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/castles/" + req.params.id);
+    }
+  });
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
